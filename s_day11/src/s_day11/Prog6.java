a0ab836482b280c7c07ff3e92ffe88e24a9a45e1
Good Luck <Roshansangapur@gmail.com>
2022-02-22 23:51:15 +0530

    day11 committed

---
+++ s_day11/src/s_day11/Prog6.java
@@ -0,0 +1,15 @@
+package s_day11;
+
+public class Prog6 {
+
+	public static void main(String[] args) {
+		 try{
+	            System.out.println(4/0);
+	           }
+	        catch(ArithmeticException e){
+	            System.out.println("divide by 0");
+	        }
+
+	}
+
+}
